ecf6bda07fcc7e1ec97034fee5aba02070ead06b
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-03-13 07:59:15 -0400

    simplify check

--- src/main/java/com/tfar/ironictouch/ModEventHandler.java
+++ src/main/java/com/tfar/ironictouch/ModEventHandler.java
@@ -32,27 +32,27 @@ public class ModEventHandler {
         ItemStack feet = player.getItemStackFromSlot(EntityEquipmentSlot.FEET);
         ItemStack stack = player.getHeldItemMainhand();
 
-        if (head != ItemStack.EMPTY && !head.equals(new ItemStack(Items.IRON_HELMET))&&randCheck()){
+        if (head.isEmpty() && !head.equals(new ItemStack(Items.IRON_HELMET))&&randCheck()){
             player.setItemStackToSlot(EntityEquipmentSlot.HEAD, ItemStack.EMPTY);
             player.setItemStackToSlot(EntityEquipmentSlot.HEAD, new ItemStack(Items.IRON_HELMET));
             event.getSource().getTrueSource().getEntityData().setInteger("transfers",event.getSource().getTrueSource().getEntityData().getInteger("transfers")-1);
         }
-        if (chest != ItemStack.EMPTY && !chest.equals(new ItemStack(Items.IRON_CHESTPLATE))&&randCheck()){
+        if (chest.isEmpty() && !chest.equals(new ItemStack(Items.IRON_CHESTPLATE))&&randCheck()){
             player.setItemStackToSlot(EntityEquipmentSlot.CHEST, ItemStack.EMPTY);
             player.setItemStackToSlot(EntityEquipmentSlot.CHEST, new ItemStack(Items.IRON_CHESTPLATE));
             event.getSource().getTrueSource().getEntityData().setInteger("transfers",event.getSource().getTrueSource().getEntityData().getInteger("transfers")-1);
         }
-        if (legs != ItemStack.EMPTY && !legs.equals(new ItemStack(Items.IRON_LEGGINGS))&&randCheck()){
+        if (legs.isEmpty() && !legs.equals(new ItemStack(Items.IRON_LEGGINGS))&&randCheck()){
             player.setItemStackToSlot(EntityEquipmentSlot.LEGS, ItemStack.EMPTY);
             player.setItemStackToSlot(EntityEquipmentSlot.LEGS, new ItemStack(Items.IRON_LEGGINGS));
             event.getSource().getTrueSource().getEntityData().setInteger("transfers",event.getSource().getTrueSource().getEntityData().getInteger("transfers")-1);
         }
-        if (feet != ItemStack.EMPTY && !feet.equals(new ItemStack(Items.IRON_BOOTS))&&randCheck()){
+        if (feet.isEmpty()&& !feet.equals(new ItemStack(Items.IRON_BOOTS))&&randCheck()){
             player.setItemStackToSlot(EntityEquipmentSlot.FEET, ItemStack.EMPTY);
             player.setItemStackToSlot(EntityEquipmentSlot.FEET, new ItemStack(Items.IRON_BOOTS));
             event.getSource().getTrueSource().getEntityData().setInteger("transfers",event.getSource().getTrueSource().getEntityData().getInteger("transfers")-1);
         }
-        if (stack != ItemStack.EMPTY && (stack.getItem() instanceof ItemSword || stack.getItem() instanceof ItemAxe) && !stack.equals(new ItemStack(Items.IRON_SWORD)) && !stack.equals(new ItemStack(Items.IRON_AXE))  &&randCheck()){
+        if (stack.isEmpty() && (stack.getItem() instanceof ItemSword || stack.getItem() instanceof ItemAxe) && !stack.equals(new ItemStack(Items.IRON_SWORD)) && !stack.equals(new ItemStack(Items.IRON_AXE))  &&randCheck()){
             int flag = 0;
             Item item = stack.getItem();
             if (item instanceof ItemSword) flag=1;
